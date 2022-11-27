4d05e49b96565c2c39f88c3506ed73f1e30e43b2
Connor Underwood <ctu266667@gmail.com>
2022-11-27 15:42:44 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.io.*;
 import java.net.*;
 
@@ -64,46 +63,7 @@ class Server {
 
         public void run()
         {
-            PrintWriter out = null;
-            BufferedReader in = null;
-            try {
-                // get the outputstream of client
-                out = new PrintWriter(
-                        clientSocket.getOutputStream(), true);
-
-                // get the inputstream of client
-                in = new BufferedReader(
-                        new InputStreamReader(
-                                clientSocket.getInputStream()));
-
-                String line;
-                while ((line = in.readLine()) != null) {
-
-                    // writing the received message from
-                    // client
-                    System.out.printf(
-                            " Sent from the client: %s\n",
-                            line);
-                    out.println(line);
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                try {
-                    if (out != null) {
-                        out.close();
-                    }
-                    if (in != null) {
-                        in.close();
-                        clientSocket.close();
-                    }
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            new Thread(new WelcomeToHappyFeet()).start();
         }
     }
 }
