12dfbda59acea55a3aef57c7d905cbb96f22c108
YadavSuhani <112599773+YadavSuhani@users.noreply.github.com>
2022-11-28 13:19:14 -0500

    added bg image

--- WelcomePageGUI.java
+++ WelcomePageGUI.java
@@ -2,21 +2,21 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class WelcomePageGUI extends JComponent implements Runnable {
+public class WelcomeToHappyFeet extends JComponent implements Runnable {
 
-    JButton logInButton;
-    JButton createAccountButton;
-    JLabel welcomeMessage;
-
-    JFrame frame;
-    final static boolean shouldFill = true;
-    final static boolean shouldWeightX = true;
+    public JButton logInButton;
+    public JButton createAccountButton;
+    public JLabel welcomeMessage;
+    public final static boolean shouldFill = true;
+    public final static boolean shouldWeightX = true;
+    public JFrame frame;
+    public Image image;
+    public final static String filePath = "image.jpg";
 
     ActionListener actionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == logInButton) {
-                dispose();
                 logIn();
             }
 
@@ -25,69 +25,96 @@ public class WelcomePageGUI extends JComponent implements Runnable {
             }
         }
     };
-    public void dispose() {
-        this.frame.dispose();
-    }
+
     public void logIn() {
-        new Thread(new Login()).start();
+        frame.dispose();
     }
 
     public void createAccount() {
-
-    }
-    public WelcomePageGUI() {
-
+        CreateAccount createAccount = new CreateAccount();
+        createAccount.run();
+        frame.dispose();
     }
 
     public void run() {
-        frame = new JFrame("Happy Feet");
-        Container pane = frame.getContentPane();
-        pane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        if (shouldFill) {
+        try {
+            image = Toolkit.getDefaultToolkit().getImage(filePath);
+            frame = new JFrame("Happy Feet");
+            frame.setContentPane(new BackGroundPanel(image));
+            Container pane = frame.getContentPane();
+            pane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+            pane.setLayout(new GridBagLayout());
+
+            welcomeMessage = new JLabel("Welcome to Happy Feet");
+            welcomeMessage.setFont(new Font("Serif", Font.PLAIN, 30));
+            logInButton = new JButton("Log In");
+            logInButton.addActionListener(actionListener);
+            createAccountButton = new JButton("Create An Account");
+            createAccountButton.addActionListener(actionListener);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setSize(600, 442);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+            GridBagConstraints c = new GridBagConstraints();
+            if (shouldFill) {
+                c.fill = GridBagConstraints.HORIZONTAL;
+            }
+
+            if (shouldWeightX) {
+                c.weightx = 0.0;
+            }
+            c.insets = new Insets(6,6,6,6);
+            JPanel textPanel = new JPanel();
+            textPanel.add(welcomeMessage);
             c.fill = GridBagConstraints.HORIZONTAL;
-        }
+            c.gridx = 0;
+            c.gridy = 0;
+            pane.add(textPanel, c);
 
-        welcomeMessage = new JLabel("Welcome to Happy Feet");
-        welcomeMessage.setFont(new Font("Serif", Font.PLAIN, 30));
-        logInButton = new JButton("Log In");
-        logInButton.addActionListener(actionListener);
-        createAccountButton = new JButton("Create An Account");
-        createAccountButton.addActionListener(actionListener);
-
-        frame.setSize(600, 400);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        if (shouldWeightX) {
+            c.fill = GridBagConstraints.HORIZONTAL;
             c.weightx = 0.0;
+            c.gridx = 0;
+            c.gridy = 1;
+            pane.add(logInButton,c);
+
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 0.0;
+            c.gridx = 0;
+            c.gridy = 2;
+            pane.add(createAccountButton,c);
+        } catch (Exception io) {
+            io.printStackTrace();
         }
-        c.insets = new Insets(6,6,6,6);
-        JPanel textPanel = new JPanel();
-        textPanel.add(welcomeMessage);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        pane.add(textPanel, c);
-
-        JPanel buttonPanel = new JPanel();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.0;
-        c.gridx = 0;
-        c.gridy = 1;
-        pane.add(logInButton,c);
-
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.0;
-        c.gridx = 0;
-        c.gridy = 2;
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pane.add(createAccountButton,c);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new WelcomePageGUI());
+        SwingUtilities.invokeLater(new WelcomeToHappyFeet());
+    }
+}
+
+class BackGroundPanel extends JPanel {
+    private final Image image;
+    public BackGroundPanel(Image image) {
+        this.image = image;
+    }
+
+    public Dimension getPreferredSize() {
+        Dimension superSize = super.getPreferredSize();
+        if (isPreferredSizeSet() || image == null) {
+            return superSize;
+        }
+
+        int prefW = Math.max(image.getWidth(null), superSize.width);
+        int prefH = Math.max(image.getHeight(null), superSize.height);
+        return new Dimension(prefW, prefH);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null);
     }
 }
