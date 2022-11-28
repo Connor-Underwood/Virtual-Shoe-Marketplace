c370d0a9bd6cf4473e07e2e5b2a30957ec41cb20
Zeyad Adham <112333080+ZeyadAdham@users.noreply.github.com>
2022-11-27 20:53:38 -0500

    Update AddStoreGUI.java

--- AddStoreGUI.java
+++ AddStoreGUI.java
@@ -8,6 +8,8 @@ public class AddStoreGUI extends JComponent implements Runnable {
     JTextField storeNameField;
     JLabel welcomeMessage;
 
+    JButton addStore;
+
     JFrame frame;
     final static boolean shouldFill = true;
     final static boolean shouldWeightX = true;
@@ -45,32 +47,36 @@ public class AddStoreGUI extends JComponent implements Runnable {
             c.fill = GridBagConstraints.HORIZONTAL;
         }
 
-        welcomeMessage = new JLabel("What is the name of the store you would like to add?");
-        welcomeMessage.setFont(new Font("Times new Roman", Font.PLAIN, 25));
+        welcomeMessage = new JLabel("Store Name:");
+        welcomeMessage.setFont(new Font("Times new Roman", Font.PLAIN, 18));
         storeNameField = new JTextField(5);
         storeNameField.addActionListener(actionListener);
+
+        c.insets = new Insets(6,6,6,6);
+
+        JPanel storePanel = new JPanel();
+        storePanel.add(welcomeMessage);
+        storePanel.add(storeNameField);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        pane.add(storePanel, c);
+
 
         frame.setSize(600, 400);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
+        addStore = new JButton("Add Store");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        pane.add(addStore, c);
+
+
         if (shouldWeightX) {
             c.weightx = 0.0;
         }
-        c.insets = new Insets(6,6,6,6);
-        JPanel textPanel = new JPanel();
-        textPanel.add(welcomeMessage);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        pane.add(textPanel, c);
-
-        JPanel buttonPanel = new JPanel();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.0;
-        c.gridx = 0;
-        c.gridy = 1;
-        pane.add(storeNameField,c);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
