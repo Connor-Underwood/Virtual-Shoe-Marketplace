529b17a25ad4eaacde043e957ffcd289cb8bb132
YadavSuhani <112599773+YadavSuhani@users.noreply.github.com>
2022-11-30 09:04:55 -0500

    Update Server.java
    Added some seller options

--- Server.java
+++ Server.java
@@ -54,6 +54,8 @@ class Server {
         }
 
         public void run() {
+            int typeOfUser = -1;
+            int userPin = -1;
             MarketPlace.loadMarket(); // loads Market for all clients
             try {
                 // Login/Create Account Implementation ----------
@@ -72,14 +74,17 @@ class Server {
                     }
                     writer.println(password); // send password back to client
 
-                    if (MarketPlace.checkPinFromCredentials(email, password).equals("Seller")) {
+                    if (MarketPlace.checkPinFromCredentials(email, password).length() == 4) {
+                        userPin = Integer.parseInt(MarketPlace.checkPinFromCredentials(email, password));
+                        typeOfUser = 1;
                         writer.println("Seller");
-                    } else {
+                    } else if (MarketPlace.checkPinFromCredentials(email, password).length() == 5) {
+                        userPin = Integer.parseInt(MarketPlace.checkPinFromCredentials(email, password));
+                        typeOfUser = 2;
                         writer.println("Customer");
                     }
                 }
                 else if (start.equals("Create")) { // Client is trying to Create An Account
-                    int pin = -1; // We will create a pin for this user later, just a placeholder value for now
                     Random random = new Random(); // Used to create the pin value for the client
                     MarketPlace.createAccountsFile(); // if accounts.csv doesn't exist, we create it here
                     String email = reader.readLine(); // receive E-Mail from Client
@@ -88,27 +93,29 @@ class Server {
                         email = reader.readLine();
                     }
                     writer.println(email); // Send valid E-Mail back to Client
-                    
+
                     String password = reader.readLine(); // Does not have to verify, this is a new Password
-                    String userType = reader.readLine(); // Receive the userType of the created account, Seller/Customer
-                    
-                    
+                    String userType = reader.readLine(); // Receive the typeOfUser of the created account, Seller/Customer
+
+
                     if (userType.equals("Seller")) { // We create the Pin for that Seller
-                        pin = random.nextInt(1000, 9999);
-                        while (!MarketPlace.checkPin(Integer.toString(pin))) { // make sure pin is not taken
-                            pin = random.nextInt(1000, 9999);
+                        typeOfUser = 1;
+                        userPin = random.nextInt(1000, 9999);
+                        while (!MarketPlace.checkPin(Integer.toString(userPin))) { // make sure pin is not taken
+                            userPin = random.nextInt(1000, 9999);
                         }
                     } else {
-                        pin = random.nextInt(10000, 99999); // We create the Pin for that Customer
-                        while (!MarketPlace.checkPin(Integer.toString(pin))) { // make sure pin is not taken
-                            pin = random.nextInt(10000, 99999);
+                        typeOfUser = 2;
+                        userPin = random.nextInt(10000, 99999); // We create the Pin for that Customer
+                        while (!MarketPlace.checkPin(Integer.toString(userPin))) { // make sure pin is not taken
+                            userPin = random.nextInt(10000, 99999);
                         }
                     }
                     synchronized (GATEKEEPER) { // THIS MUST BE SYNCHRONIZED BECAUSE WE ARE ACCESSING SHARED INFORMATION
-                        // FOR ALL CLIENTS. A.K.A, ALL CLIENTS ARE READING FROM THIS FILE, MAKE SURE ONLY ONE CLIENT WRITES 
+                        // FOR ALL CLIENTS. A.K.A., ALL CLIENTS ARE READING FROM THIS FILE, MAKE SURE ONLY ONE CLIENT WRITES
                         // TO IT AT A TIME
                         try (PrintWriter writer = new PrintWriter(new FileWriter("accounts.csv", true))) {
-                            writer.println(pin + "," + email + "," + password + ",");
+                            writer.println(userPin + "," + email + "," + password + ",");
                             writer.flush();
                         } catch (IOException io) {
                             System.out.println("Error writing to the accounts.csv file.");
@@ -117,29 +124,87 @@ class Server {
 
                     if (userType.equals("Seller")) {
                         synchronized (GATEKEEPER) { // THIS MUST BE SYNCHRONIZED BECAUSE WE ARE ACCESSING SHARED INFORMATION
-                            // FOR ALL CLIENTS. A.K.A, ALL CLIENTS ARE READING FROM THIS FILE, MAKE SURE ONLY ONE CLIENT WRITES 
+                            // FOR ALL CLIENTS. A.K.A., ALL CLIENTS ARE READING FROM THIS FILE, MAKE SURE ONLY ONE CLIENT WRITES
                             // TO IT AT A TIME
                             try (PrintWriter writer = new PrintWriter(new FileWriter("market.csv", true))) {
-                                writer.println(pin + "," + email + "," + password + ",");
+                                writer.println(userPin + "," + email + "," + password + ",");
                                 writer.flush();
                             } catch (IOException io) {
                                 System.out.println("Error writing to the market.csv file.");
                             }
-                            Seller seller = new Seller(Integer.toString(pin), email, password); // make a new Seller object
+                            Seller seller = new Seller(Integer.toString(userPin), email, password); // make a new Seller object
                             MarketPlace.sellers.add(seller); // add it to the arrayList of Sellers because they just made an account
                         }
                     } else {
                         synchronized (GATEKEEPER) { // THIS MUST BE SYNCHRONIZED BECAUSE WE ARE ACCESSING SHARED INFORMATION
-                            // FOR ALL CLIENTS. A.K.A, ALL CLIENTS OBTAIN INFO FROM THE STATIC ARRAYLIST OF CUSTOMERS
+                            // FOR ALL CLIENTS. A.K.A., ALL CLIENTS OBTAIN INFO FROM THE STATIC ARRAYLIST OF CUSTOMERS
                             // IN MARKETPLACE, IF WE MODIFY IT LIKE WE ARE DOING HERE, IT MUST BE SYNCHRONIZED
-                            Customer customer = new Customer(Integer.toString(pin), email, password);
+                            Customer customer = new Customer(Integer.toString(userPin), email, password);
                             MarketPlace.customers.add(customer);
                         }
                     }
                     // End of Login/Create Account Implementation ----------
-
-
                 }
+
+                // START OF SELLER IMPLEMENTATION;
+                if (typeOfUser == 1) {
+                    int index;
+                    Seller seller;
+                    synchronized (GATEKEEPER) {
+                        index = -1;
+                        for (Seller s : MarketPlace.sellers) {
+                            if (s.getPin().equals(Integer.toString(userPin))) {
+                                index = MarketPlace.sellers.indexOf(s);
+                            }
+                        }
+                        seller = MarketPlace.sellers.get(index);
+                    }
+
+                    String sellerSelectedOption = reader.readLine();
+
+                    if (sellerSelectedOption.equalsIgnoreCase("Add a store")) {
+                        String storeName = reader.readLine();
+                        Store tempStore;
+                        synchronized (GATEKEEPER) {
+                            tempStore = new Store(storeName, seller);
+                            if (seller.addStore(tempStore, true)) {
+                                writer.println("Store added");
+                            } else {
+                                writer.println("Store already exists");
+                            }
+                            MarketPlace.sellers.set(index, seller);
+                        }
+                    }
+
+                    if (sellerSelectedOption.equalsIgnoreCase("Add a shoe")) {
+                        String storeName = reader.readLine();
+                        int storeIndex = -1;
+                        synchronized (GATEKEEPER) {
+                            for (int i = 0; i < seller.getStores().size(); i++) {  // SHOULD WE SYNCHRONIZE THIS??????
+                                if (seller.getStores().get(i).getName().equals(storeName)) {
+                                    storeIndex = i;
+                                }
+                            }
+                            writer.println(storeIndex);
+                            writer.flush();
+                        }
+                        Store store = seller.getStores().get(storeIndex);
+                        String shoeName = reader.readLine();
+                        String shoeDesc =  reader.readLine();
+                        double price = Double.parseDouble(reader.readLine());
+                        int quantity = Integer.parseInt(reader.readLine());
+                        synchronized (GATEKEEPER) {
+                            Shoe shoe = new Shoe(store, shoeName, shoeDesc, price, quantity);
+                            if (seller.addShoe(store, shoe, true)) {
+                                MarketPlace.sellers.set(index, seller);
+                                writer.println("Shoe added");
+                            } else {
+                                writer.println("Shoe could not be added");
+                            }
+                        }
+                    }
+                }
+
 
 
 
